bb8c936113fc7da27a42eb1b5620c9aa04a52ccf
koldin108902 <alex108902@naver.com>
2022-06-20 06:17:36 +0900

    update: bot's msg filter

--- router/route.go
+++ router/route.go
@@ -10,6 +10,9 @@ func Route(w *discordgo.Session, r *discordgo.MessageCreate) {
 	if len(r.Content) < 1 {
 		return
 	}
+	if r.Author.Bot {
+		return
+	}
 	msg := strings.Split(r.Content, "")
 
 	if msg[0] != "$" {
@@ -22,5 +25,8 @@ func Route(w *discordgo.Session, r *discordgo.MessageCreate) {
 	}
 	if cmd[0] == "종료" {
 		StopRepeat(w, r)
+	}
+	if cmd[0] == "설명" {
+		CmdHelp(w, r)
 	}
 }
